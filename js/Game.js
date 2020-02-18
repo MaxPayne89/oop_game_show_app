e21298e771ff027ed0dd4aa3d9a38f625ddf3e20
maximilianpergl <maximilian.pergl@eon-energie.com>
2020-02-18 08:10:52 +0100

    state of development

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,43 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor(){
+        this.missed = 0,
+        this.phrases = this.createPhrases(),
+        this.activePhrase = null
+    }
+
+    startGame(){
+        //hide overlay
+        document.querySelector('div#overlay').style.visibility = 'hidden';
+        const phrase = this.getRandomPhrase();
+        this.activePhrase = phrase;
+        phrase.addPhraseToDisplay();
+    }
+
+    getRandomPhrase(){
+        let arr = this.phrases
+        let rand = Math.round(Math.random() * 4)
+        return arr[rand]
+    }
+
+    handleInteraction(){}
+
+    removeLife(){}
+
+    checkForWin(){}
+
+    gameOver(){}
+
+    //create some phrases to be used in the game
+    createPhrases(){
+        let arrOfPhrases = []; 
+        arrOfPhrases.push(new Phrase("Do your Job"));
+        arrOfPhrases.push(new Phrase("Mo money, mo problems"));
+        arrOfPhrases.push(new Phrase("Just do it"));
+        arrOfPhrases.push(new Phrase("What would Kobe do"));
+        arrOfPhrases.push(new Phrase("We talking about practice"));
+        return arrOfPhrases;
+    }
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,25 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase()
+    }
+
+    addPhraseToDisplay(){
+        const arr = this.phrase.split("");
+        const arrReducer = arr.reduce((acc, curr) => {
+            if(curr !== ' '){
+                return acc + `<li class="hide letter ${curr}">${curr}</li>`
+            } else {
+                return acc + `<li class="space">${curr}</li>`
+            }
+        }, "")
+        const phraseDiv = document.getElementById("phrase")
+        phraseDiv.querySelector('ul').insertAdjacentHTML("beforeend",arrReducer);
+    }
+
+    checkLetter(){}
+
+    showMatchedLetter(){}
+}
