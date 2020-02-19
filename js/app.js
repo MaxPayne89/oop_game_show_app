403a7f8b8c48d31c4d1df76b54291753f8737590
maximilianpergl <maximilian.pergl@eon-energie.com>
2020-02-19 10:47:18 +0100

    implemented final step

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ let game;
 const buttonStartGame = document.querySelector("button#btn__reset");
 buttonStartGame.addEventListener('click', () => {
     game = new Game();
-    game.startGame()
+    //clean up before starting a new game
+    removePhraseFromDisplay();
+    enableAllButtons();
+    resetHearts();
+    //start new game
+    game.startGame();
 });
 
 
@@ -17,3 +22,26 @@ keyboard.addEventListener('click', (e) => {
         game.handleInteraction(e.target);
     }
 });
+
+//helper methods
+const removePhraseFromDisplay = () => {
+    const ulWithChars = document.querySelector('div#phrase > ul');
+    ulWithChars.innerHTML = '';
+}
+
+const enableAllButtons = () => {
+    const buttons = document.querySelectorAll('div.keyrow > button');
+    const buttonsArr = Array.from(buttons);
+    buttonsArr.map(button => {
+        button.classList = 'key';
+        button.disabled = false;
+    })
+}
+
+const resetHearts = () => {
+    const hearts = document.querySelectorAll('.tries > img');
+    const heartsArr = Array.from(hearts);
+    heartsArr.map(heart => {
+        heart.setAttribute('src', 'images/liveHeart.png');
+    })
+}
